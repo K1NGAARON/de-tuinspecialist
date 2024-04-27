054bd9776197dce36546a4cdc150df2d57d94df8
K1NGAARON <gerardaaron1997@gmail.com>
2024-04-27 14:42:12 +0200

    changes

--- home/js/script.js
+++ home/js/script.js
@@ -1,16 +1,16 @@
 function changeReviews() {
     const reviews = [
         {
-            name: "Sidney",
-            review: "Just wanna say thanks a lot man, before I subscribed to you I was recklessly betting and losing abit of money but after i subscribed, I learnt not to recklessly bet, listened to you and it bailed out. You're the best man thank you",
+            name: "Klant x",
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur rerum quae aut quo deleniti repellendus tempore voluptatem, tenetur id omnis.",
         },
         {
-            name: "John P.",
-            review: "Just wanted to say this is the best group I've been with, won many times today biggest ACCA being £224, keep up the good wood, P.s I've gone into the elite tier now, well impressed"
+            name: "Klant x",
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur rerum quae aut quo deleniti repellendus tempore voluptatem, tenetur id omnis.",
         },
         {
-            name: "Will",
-            review: "PSA is truly worth it, especially elite. Been elite now for two months and have profited more than I could have ever imagined. Paid for my Holiday to Greece for 2 already!",
+            name: "Klant x",
+            review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur rerum quae aut quo deleniti repellendus tempore voluptatem, tenetur id omnis.",
         },
     ];
 
